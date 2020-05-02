cb7877e604d797aa8595f5b79ae9a60bc6ec3abb
Karndeep <Kraibhat@eng.ucsd.edu>
2020-05-02 13:54:28 -0700

    Create read_scans.py

---
+++ read_scans.py
@@ -0,0 +1,26 @@
+
+# import os
+# import pydicom
+# import matplotlib.pyplot as plt
+
+# data_directory = 'C:/Users/19095/Documents/ECE228/NBIA_CT_Data/LCTSC/LCTSC-Test-S1-101/03-03-2004-08186/79262'
+
+
+# # scan_files = os.listdir(data_directory)
+
+# # for file in scan_files:
+# #     scan = data_directory + '/' + file
+# #     data = pydicom.dcmread(scan)
+# #     plt.imshow(data.pixel_array, cmap = plt.cm.bone)
+# #     plt.show()
+    
+
+# segmentation_directory = "C:/Users/19095/Documents/ECE228/NBIA_CT_Data/LCTSC/LCTSC-Test-S1-101/03-03-2004-08186/1.000000-56597/1-1.dcm"
+
+# data = pydicom.dcmread(segmentation_directory)
+# contours = data.ROIContourSequence
+
+
+
+# SOURCE: http://aapmchallenges.cloudapp.net/forums/3/2/
+
